8114f1fa9b354f37f8cbaa88ae4ac1680a0b8d7c
unknown <omar.navarro@g.austincc.edu>
2020-02-27 20:13:27 -0600

    fix syntax error at dependencies

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -13,31 +13,35 @@ namespace pluralsightfuncs
   public static class OnPaymentReceived
   {
     [FunctionName("OnPaymentReceived")]
-
-    // where MyAppSetting is the name of the application which contains connection string
-    // Queue("orders"), Connection="MyAppSetting"
-
     public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, post", Route = null)] HttpRequest req,
-            [Queue("orders")] IAsyncCollector<Order> orderQueue,
-            ILogger log)
-        {
-      log.LogInformation("Received a payment");
-
-
-
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+        [Queue("orders")] IAsyncCollector<Order> orderQueue,
+        [Table("orders")] IAsyncCollector<Order> orderTable,
+        ILogger log)
+    {
+      log.LogInformation("Received a payment.");
       string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-      // de-serialize request
       var order = JsonConvert.DeserializeObject<Order>(requestBody);
-
-      // dynamic data = JsonConvert.DeserializeObject(requestBody);
-
       await orderQueue.AddAsync(order);
 
-      Logger.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
+      order.PartitionKey = "orders"; // just one partition (for demo purposes)
+      order.RowKey = order.OrderId;
+      await orderTable.AddAsync(order);
 
+      log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
       return new OkObjectResult($"Thank you for your purchase");
     }
   }
+
+  public class Order
+  {
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+
+    public string OrderId { get; set; }
+    public string ProductId { get; set; }
+    public string Email { get; set; }
+    public decimal Price { get; set; }
+  }
+
 }
